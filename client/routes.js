ea4bdebdf8826d0385da3cffdf5715fdbbee7d3e
Tibo <thibaut.gaudier78@gmail.com>
2020-06-04 12:55:34 +0200

    standard.rive

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/public', express.static('public'));
-router.use('/api', require('./api.js'))
 
 router.use(function(req, res) {
     res.render("home.ejs")
