c9339ffddc70550b8b0159a14d169258a11e4523
Tibo <thibaut.gaudier78@gmail.com>
2020-06-06 20:49:01 +0200

    merge

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const Discord = require("discord.js")
 
 const Bot = require('./Bot.js')
 const Bots = require('./Bots.js')
