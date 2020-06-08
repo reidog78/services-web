395f625c08e142e616ef37fc634853b4a62d0725
Tibo <thibaut.gaudier78@gmail.com>
2020-06-08 14:21:05 +0200

    correction {}

--- api/api.js
+++ api/api.js
@@ -27,8 +27,7 @@ router.get('/chatbots', cors(corsOptions), function(req, res) {
 router.post('/chatbots', cors(corsOptions), function(req, res) {
     if (req.is('json')) {
         let bot = bots.addBot(req.body)
-        let envoi = {}
-        Object.assign(envoi, bot)
+        let envoi = Object.assign({}, bot)
         envoi.bot = {}
         Bot.nextId++;
         res.setHeader('Content-Type', 'application/json')
@@ -42,7 +41,7 @@ router.post('/chatbots', cors(corsOptions), function(req, res) {
 router.get('/chatbots/:idBot', cors(corsOptions), function(req, res) {
     let bot = bots.getBot(parseInt(req.params.idBot))
     if (bot != undefined) {
-        let send = Object.assign(send, bot)
+        let send = Object.assign({}, bot)
         send.bot = {}
         res.setHeader('Content-Type', 'application/json')
         res.json(send)
